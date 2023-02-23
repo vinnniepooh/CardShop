e1ab0fa2e2c4100e3a214e6a8e3f92f3ae04040e
vnguyen <vnguyen@WS021086>
2023-02-23 12:11:29 +0100

    Projektdateien hinzufügen.

---
+++ CardShop.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CardShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CardShop.DataAccess
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+            
+        DbSet<Category> Categories { get; set; }
+    }
+}
