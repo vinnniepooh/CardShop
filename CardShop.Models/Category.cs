314ca3f8cbd2b9998f6a8a36a9af0280ca010b70
vinnniepooh <vinh.n@me.com>
2023-02-24 15:46:02 +0100

    third

--- CardShop.Models/Category.cs
+++ CardShop.Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CardShop.Models
 {
@@ -8,7 +9,8 @@ namespace CardShop.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage ="Display Order must be within range of 1 and 100")]
         public int DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
--- CardShop/Controllers/CategoryController.cs
+++ CardShop/Controllers/CategoryController.cs
@@ -25,10 +25,103 @@ namespace CardShop.Controllers
 
             return View();
         }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                //Custom Error Message
+                ModelState.AddModelError("name", "The Display Order cannot match excactly the name.");
+            }
+            if (ModelState.IsValid) 
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var CategoryFromDb = _db.Categories.Find(id);
+
+            //var CategoryFromDb = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(CategoryFromDb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                //Custom Error Message
+                ModelState.AddModelError("name", "The Display Order cannot match excactly the name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["update"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var CategoryFromDb = _db.Categories.Find(id);
+
+            //var CategoryFromDb = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(CategoryFromDb);
+        }
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var CategoryFromDb = _db.Categories.FirstOrDefault(x => x.Id == id);
+            if (id == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(CategoryFromDb);
+            _db.SaveChanges();
+            TempData["delete"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+
+            return View(id);
+        }
 
         public IActionResult Details()
         {
-            
+
             return View();
         }
         public IActionResult Error()
