5af445e21b6d414fc68af3383cacc7f0e756b349
vinh.n@me.com <vinh.n@me.com>
2023-04-25 00:12:07 +0200

    eighth

---
+++ CardShop.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,55 @@
+﻿using CardShop.DataAccess.Repository.IRepository;
+using CardShop.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardShop.DataAccess.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<Category> dbSet;
+        public CategoryRepository(ApplicationDbContext db, DbSet<Category> category)
+        {
+            _db = db;
+            this.dbSet = _db.Set<Category>();                                       
+        }
+        public void Add(Category entity)
+        {
+           dbSet.Add(entity);
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            IQueryable<Category> query = dbSet;
+            return query.ToList();
+        }
+
+        public Category GetFirstOrDefault(Expression<Func<Category, bool>> filter)
+        {
+            IQueryable<Category> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(Category entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<Category> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+
+        public void Update(Category entity)
+        {
+            dbSet.Update(entity);
+        }
+    }
+}
---
+++ CardShop.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using CardShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardShop.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        IEnumerable<Category> GetAll();
+        void Add(Category entity);
+        void Update(Category entity);
+        void Remove(Category entity);
+        Category GetFirstOrDefault(Expression<Func<Category, bool>> filter);
+        void RemoveRange(IEnumerable<Category> entity);
+    }
+}
